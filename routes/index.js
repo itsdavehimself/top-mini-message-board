125f23696af374ffec085b6839472f0adad8a501
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-27 15:11:32 +0200

    Connect database to allow rendering and posting

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,39 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const router = express.Router();
 
-const messages = [
-  {
-    text: 'Hi there',
-    user: 'Amando',
-    added: new Date(),
-  },
-  {
-    text: 'Hello world!',
-    user: 'Charles',
-    added: new Date(),
-  },
-];
+const uri = process.env.SERVER_URL;
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Mini Messageboard', messages });
+const client = new MongoClient(uri);
+
+client.connect()
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+router.get('/', async (req, res, next) => {
+  try {
+    const messagesCollection = client.db('mini_message_board').collection('messages');
+    const messages = await messagesCollection.find({}).toArray();
+
+    res.render('index', { title: 'Mini Messageboard', messages });
+  } catch (err) {
+    console.error('Error:', err);
+    next(err);
+  }
 });
 
-router.post('/new', (req, res) => {
+router.post('/new', async (req, res) => {
   const { messageText } = req.body;
   const { userName } = req.body;
-  messages.push({ text: messageText, user: userName, added: new Date() });
+  const messagesCollection = client.db('mini_message_board').collection('messages');
+  await messagesCollection.insertOne({ text: messageText, user: userName, added: new Date() });
+
   res.redirect('/');
 });
 
